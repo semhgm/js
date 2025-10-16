6b59cf8d5a37b328ae7f8bf813c702bd8764f368
semih <215541020@firat.edu.tr>
2025-10-16 14:13:15 +0300

    Add yazdir and checkAge functions for user input and age validation

--- 3/app.js
+++ 3/app.js
@@ -5,3 +5,20 @@ c = a + b;
 console.log(c);
 
 
+function yazdir(isim, soyisim) {
+    console.log("İsim: " + isim + " Soyisim: " + soyisim);
+    return "İsim: " + isim + " Soyisim: " + soyisim;
+}
+yazdir("semih","gümüş");
+metin = yazdir("semih","gümüş");
+document.write(metin);
+
+
+function checkAge(age) {
+let yas = prompt("Yaşınızı giriniz: ");
+if (yas < 18) {
+    alert("Reşit değilsiniz.");
+} else {
+    alert("Reşitsiniz.");
+}
+}
