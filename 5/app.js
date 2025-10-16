167cbb8138d1af62bf159fa5ee4eb93717eabe8d
semih <215541020@firat.edu.tr>
2025-10-16 15:17:39 +0300

    Refactor code to comment out letter frequency counting logic and add perfect number checking functionality

--- 5/app.js
+++ 5/app.js
@@ -1,15 +1,42 @@
 
 // kullanıcıdan bir harf al ve metin içinde kaç kere geçtiğini bul
 
-let metin = "ben semih yazılım mühendisliği öğrencisiyim ve halihazırda web teknolojileri ile ilgileniyorum";
+// const { is } = require("express/lib/request");
 
-const harf = prompt("Aramak istediğiniz harfi giriniz: ");
+// let metin = "ben semih yazılım mühendisliği öğrencisiyim ve halihazırda web teknolojileri ile ilgileniyorum";
 
-let sayac = 0;
-for (let i = 0; i<metin.length; i++){
+// const harf = prompt("Aramak istediğiniz harfi giriniz: ");
 
-    if(metin[i].toLowerCase() === harf.toLowerCase()){
-        sayac++;
+// let sayac = 0;
+// for (let i = 0; i<metin.length; i++){
+
+//     if(metin[i].toLowerCase() === harf.toLowerCase()){
+//         sayac++;
+//     }
+// }
+// alert(`Aradığınız harf "${harf}" metinde ${sayac} kez geçiyor.`);
+
+
+// is number perfect number
+
+function isPerfectNumber(num) {
+
+    let sum = 0;
+    for (let i = 2; i <= num / 2; i++) {
+        if(num %i ===0){
+            sum +=i;
+        };
     }
+    if(sum + 1 + num ===num*2){
+        console.log(`${num} mükemmel sayıdır.`);
+    }
+    else{
+        console.log(`${num} mükemmel sayı değildir.`);
+    }
+    
 }
-alert(`Aradığınız harf "${harf}" metinde ${sayac} kez geçiyor.`);
+isPerfectNumber(28);
+isPerfectNumber(6);
+isPerfectNumber(12);
+isPerfectNumber(496);
+isPerfectNumber(8128);
