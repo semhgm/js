18493dd0e1f3bd927f3c8add330ae064a6718ab0
semih <215541020@firat.edu.tr>
2025-10-16 10:34:32 +0300

    Add string reversal and palindrome checking functions

--- 4/app.js
+++ 4/app.js
@@ -1,1 +1,42 @@
-document.write("Merhaba Dünya");
+
+
+// tersine çevir
+
+function reverseString(metin){
+
+  return metin.split("").reverse().join("");
+}
+  console.log(reverseString("Merhaba Dünya"));
+
+
+
+
+
+
+function isPalindrome(s) {
+  const clean = s.toLowerCase().replace(/[^a-z0-9ığüşöçı]/g, "");
+  let i = 0, j = clean.length - 1;
+  while (i < j) {
+    if (clean[i] !== clean[j]) return false;
+    i++; j--;
+  }
+  return true;
+}
+console.log(isPalindrome("Ey edip Adana'da pide ye")); // true
+
+
+function isPalindrome2(s){
+  const clean = s.toLowerCase().replace(/[^a-z0-9ığüşöçı]/g, "");
+  let i = 0 , j = clean.length - 1;
+  while(i < j){
+    if(clean[i] !== clean[j]) return false;
+    i++; j--;
+  }
+  return true;
+
+}
+console.log(isPalindrome2("Ey edip Adana'da pide ye")); // true
+
+
+
+
