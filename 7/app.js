faddce4f6411c71ca0cae572d72733aaf5fd1882
semhgm <semihgms65@gmail.com>
2026-05-08 13:19:40 +0300

    Add initial HTML and JavaScript files for basic interaction and arithmetic operations

---
+++ 7/app.js
@@ -0,0 +1,16 @@
+console.log("Hello World!");
+
+
+function sayHelloo(){
+    document.write("Hello World!");
+}
+document.getElementById("clickButton").addEventListener("click", sayHelloo);
+a = 5;
+b = 10
+
+function add(a, b){
+    return a + b;
+}
+console.log(add(a, b)); 
+console.log(document.location.host)
+alert("iki sayının toplamı: " + add(a, b));
